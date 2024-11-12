3a9cb180fb9484477a6e96cc05665c9534c612e2
quoc10973 <rumble01667057271@gmail.com>
2024-11-13 03:30:51 +0700

    user Module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TestController } from './controller/testController';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @Get() //Decorators are used to define the metadata for the class
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/quoc')
+  quocHandsome(): object {
+    return { quoc: 'handsome' };
+  }
+
 }
