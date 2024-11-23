9a921a8586b7ebad50646dd5b5f30ebd1c79732c
quoc10973 <rumble01667057271@gmail.com>
2024-11-24 00:30:43 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const testConnectionService = app.get(TestConnectionService);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   console.log('Testing database connection...');
   await testConnectionService.testConnection();
   await app.listen(process.env.PORT ?? 9999);
